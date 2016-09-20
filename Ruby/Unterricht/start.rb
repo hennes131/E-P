bbd46ae3def7381116f2f0ead5ba75a84993c160
hennes131 <hennes131@googlemail.com>
2016-09-20 12:55:06 +0200

    methoden

--- Ruby/Unterricht/start.rb
+++ Ruby/Unterricht/start.rb
@@ -1,12 +1,13 @@
-var = "Mikey loves Goonies"
-
-puts 'Mickey\'s favorite movie is Goonies'
-
-movie = "Goonies"
-
-puts 'Mickey\'s favorite movie is '+ movie 
-
-rank = 10 
-
-puts "#{movie} has a rank of #{rank * 2} am #{Time.now}"
+def movie_listing(movie, rank=5)
+	"Movie: #{movie} ist ein guter Film, mit der Punktzahl: #{rank} am #{set_time}"
+	
+end
+def method_name
+	
+end
+def set_time
+	Time.now.strftime("%d.%m.%Y")
+	
+end
+puts movie_listing(ARGV[0], ARGV[1])
 
