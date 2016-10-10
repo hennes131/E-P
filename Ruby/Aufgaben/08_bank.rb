4a10fdaa829c101e19c01c9a0b91fe9849df1343
hennes131 <hennes131@googlemail.com>
2016-10-10 10:11:47 +0200

    achter

--- Ruby/Aufgaben/08_bank.rb
+++ Ruby/Aufgaben/08_bank.rb
@@ -28,3 +28,44 @@
 #	'Theo Sonnenschein, Hermelinweg 11, 22159 Hamburg. Jahresgehalt: 24000 Euro.' 
 #c. Fügen Sie eine Eigenschaft kredit hinzu, auf die lesend und schreibend zugegriffen werden kann.
 #	 Speichern Sie für das bestehende Objekt in der Eigenschaft kredit den Wert 5000. 
+
+class Kredit
+
+	attr_accessor :kredit
+
+	def initialize(kunde, jahresgehalt)
+		@kunde = kunde
+		@Jahresgehalt = jahresgehalt
+	end
+
+	def get_kunde
+		puts "#{@kunde.get_kunden_daten} Jahresgehalt: #{@Jahresgehalt}"
+	end
+end
+
+class Kunde
+
+	attr_accessor :name, :gehalt
+	attr_writer :adresse
+
+
+		def initialize(vorname="", name="")
+			@name = "#{vorname} ".capitalize + "#{name}".capitalize
+		end
+
+		def get_kunden_daten
+			"#{@name}, #{@adresse}."
+		end
+end
+
+kunde1 = Kunde.new("Theo", "Sommer")
+kunde1.name = "Theo Sonnenschein"
+puts kunde1.name
+kunde1.gehalt = "2000"
+kunde1.adresse = "Hermelinweg 11, 22159 Hamburg"
+
+puts kunde1.get_kunden_daten
+
+kredit1 = Kredit.new(kunde1, "24000")
+kredit1.get_kunde
+kredit1.kredit = "5000"
--- Ruby/Aufgaben/08_interacting.rb
+++ Ruby/Aufgaben/08_interacting.rb
@@ -1,4 +1,5 @@
-#a. erstellen Sie eine Klasse Game mit einem Konstruktor, der eine Instanzvariable title anlegt und fuellt und ein leeres Array mit dem Name Player enthaelt.
+#a. erstellen Sie eine Klasse Game mit einem Konstruktor, der eine Instanzvariable title anlegt und
+#  fuellt und ein leeres Array mit dem Name Player enthaelt.
 #b. die Klasse Game hat eine Methode add_player der ein SPieler hinzugefuegt wird
 #c. eine Methode play erzeugt folgenden Ausdruck:
 #	There are 3 players in Knuckleheads: 
@@ -17,4 +18,77 @@
 #	Curly got w00ted!
 #	Curly got w00ted!
 #I'm Curly with a health of 145 and a score of 150.
+class Game
+		def initialize(title)
+			@title = title
+			@Player = []
+		end
+		def add_player(player)
+			@Player << player
+		
+		end
+		def play
+			puts "There are #{@Player.size} Players in #{@title}\n"
 
+				@Player.each do |player|
+					puts player.say_hello
+				end
+				@Player.each do |player|
+					puts player.blam
+					puts player.woot
+					puts player.woot
+					puts player.say_hello
+				end
+		end
+end
+
+
+
+
+class Player
+	
+	
+	def initialize(name, health=100) 
+		@name = name
+		@health = health
+		
+	end
+
+	def say_hello
+		"I'm #{@name} with a health of #{@health} and a score of #{score}"
+	end
+
+
+
+	def blam
+			@health -= 10
+				if @health <= 0
+					@health = 0
+						"#{@name} got blamed"
+					
+				else
+					"#{@name} got blamed"
+		end
+	end
+
+	def woot
+		@health += 10
+			"#{@name} got wooted"
+	end
+
+	def score
+		@health + @name.length
+	end
+
+
+end
+
+
+player1 = Player.new("Hans",24)
+player2 = Player.new("Max",12)
+
+
+Game1 = Game.new("Game")
+Game1.add_player(player1)
+Game1.add_player(player2)
+Game1.play
